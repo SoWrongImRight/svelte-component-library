6f6a1b4a97ef6eadea21447a41e700d673c279b5
Russ Carroll <info@rcarroll.dev>
2021-10-09 16:11:38 -0400

    Initial Accordion but this  one removes infomration from the DOM so is not ideal under all circumstances.

---
+++ src/lib/Accordion.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { get_all_dirty_from_scope } from 'svelte/internal';
+
+	import { slide } from 'svelte/transition';
+	export let buttonText = '';
+	export let isOpen = true;
+</script>
+
+<div>
+	<button on:click={() => (isOpen = !isOpen)}>
+		{buttonText}
+	</button>
+	{#if isOpen}
+		<div transition:slide>
+			<slot />
+		</div>
+	{/if}
+</div>
