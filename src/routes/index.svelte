92af914221b76011815800b3d893ebb6e75a14c5
Russ Carroll <info@rcarroll.dev>
2021-10-09 15:52:35 -0400

    Completed the Toggle component.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,15 @@
 <script>
 	import Toggle from '$lib/Toggle.svelte';
+
+	let isToggled = false;
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<Toggle />
+<Toggle bind:isToggled label="beta" />
+<Toggle bind:isToggled label="beta" style="--toggleBackgroundColor: red;" />
 
+{#if isToggled}
+	<h1>Hi, I'm toggled</h1>
+{/if}
