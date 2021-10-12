df42de8ab5a7c54182f42a59ebb4816a55457363
Russ Carroll <info@rcarroll.dev>
2021-10-12 07:41:03 -0400

    Improved the basic SearchFilter with some animation and CSS.

--- src/lib/SearchFilter.svelte
+++ src/lib/SearchFilter.svelte
@@ -1,6 +1,9 @@
 <script>
+  import { fade } from 'svelte/transition'
+
   export let items;
-  let search = '';
+  export let search = '';
+  let isFocused = false;
 
   $: filteredSearch = items.filter((item) => {
     return search === "" || item.toLowerCase().indexOf(search.toLowerCase()) !== -1;
@@ -8,13 +11,45 @@
   } )
 </script>
 
+<div class="wrapper">
 <label>
   Search Items: <br />
-  <input bind:value={search} type="text" placeholder="Search">
+  <input on:focus={() => (isFocused = true)} bind:value={search} type="text" placeholder="Search">
 </label>
 
-<ul>
-  {#each filteredSearch as item}
-    <li>{item}</li>
-  {/each}
-</ul>
+{#if isFocused}
+  <ul transition:fade={{ duration: 200}}>
+    {#each filteredSearch as item}
+      <li 
+        transition:fade={{duration: 200}}
+        on:click={() => {
+        search = item;
+        isFocused = false;
+      }}>{item}</li>
+    {/each}
+  </ul>
+{/if}
+</div>
+
+<style>
+  .wrapper {
+    position: relative;
+    display: inline-block;
+  }
+  ul {
+    position: absolute;
+    width: 100%;
+    border: solid 1px black;
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+  li {
+    padding: 10px;
+    cursor: pointer;
+    transition: 0.3s background ease;
+  }
+  li:hover {
+    background: var(--listItemBackground, #eee);
+  }
+</style>
